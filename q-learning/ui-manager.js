6396dce7a0273816e52a0fdaab9523f306c0baeb
Mihai Ionescu <mihai.t.ionescu@gmail.com>
2024-05-02 16:49:07 -0400

    add UIManager to handle user interactions

---
+++ q-learning/ui-manager.js
@@ -0,0 +1,58 @@
+class UIManager {
+  constructor() {
+    this.board = BoardFactory.createLevel_1();
+    this.renderer = new Renderer(this.board, { cellSize: 50 });
+
+    // this.isRunning = false;
+    // this.isTraining = false;
+    // this.isTrainingPaused = false;
+    
+    // this.isEpisodeOver = false;
+    // this.isEpisodeStarted = false;
+    // this.isEpisodePaused = false;
+    // this.isEpisodeCompleted = false;
+    
+    // this.isAgentDead = false;
+    // this.isAgentAtGoal = false;
+    
+    // this.isAgentMoving = false;
+    // this.isAgentMovingUp = false;
+    // this.isAgentMovingRight = false;
+    // this.isAgentMovingDown = false;
+    // this.isAgentMovingLeft = false;
+  }
+
+  initialize() {
+
+
+    this.initializeDebugUI();
+  }
+
+  initializeDebugUI() {
+    const buttonMoveAgentUp = document.getElementById("btn--move-agent-up");
+    buttonMoveAgentUp.onclick = () => this.board.agent.moveUp();
+
+    const buttonMoveAgentRight = document.getElementById("btn--move-agent-right");
+    buttonMoveAgentRight.onclick = () => this.board.agent.moveRight();
+
+    const buttonMoveAgentDown = document.getElementById("btn--move-agent-down");
+    buttonMoveAgentDown.onclick = () => this.board.agent.moveDown();
+
+    const buttonMoveAgentLeft = document.getElementById("btn--move-agent-left");
+    buttonMoveAgentLeft.onclick = () => this.board.agent.moveLeft();
+
+    [ buttonMoveAgentUp,
+      buttonMoveAgentRight,
+      buttonMoveAgentDown,
+      buttonMoveAgentLeft
+    ].forEach(button => button.className = BUTTON_CLASSES);
+  }
+}
+
+// ================================================================================================
+// STYLES
+// ================================================================================================
+
+const BUTTON_CLASSES = Object.freeze(
+  "transition bg-yellow-900 hover:bg-yellow-950 text-white font-bold py-2 px-4 rounded"
+);
