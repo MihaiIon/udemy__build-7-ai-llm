d750fda4ac745f45413dfe17abb4bfde1d518e1d
Mihai Ionescu <mihai.t.ionescu@gmail.com>
2024-05-03 14:05:10 -0400

    add QTableUIManager to handle the display of q-values

--- q-learning/simulation-manager.js
+++ q-learning/simulation-manager.js
@@ -1,6 +1,7 @@
 class SimulationManager {
   constructor() {
     this.board = BoardFactory.createLevel_1();
+
     this.renderer = new Renderer(this.board);
     this.qLearning = new QLearning(this.board, { epsilon: 0.1, learningRate: 0.1, discountFactor: 0.9 });
     this.uiManager = new UIManager(this.board, this.qLearning.qTable);
